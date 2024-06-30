2a39e2d9b8b27f80fd3fcb1d458d454eee1be78d
enzhong-wang <enzhong.wang.jr@gmail.com>
2024-06-29 22:08:57 -0400

    add logic_2

--- Python/logic_2.py
+++ Python/logic_2.py
@@ -1,12 +1,8 @@
-def make_bricks(small, big, goal):
-  needed = 0
-  
-  if goal>=big*5:
-    needed = goal-big*5
-  else:
-    needed = goal%5
-    
-  return True if small>=needed else False
+"""We want to make a row of bricks that is goal inches long. We have a number 
+of small bricks (1 inch each) and big bricks (5 inches each). Return True 
+if it is possible to make the goal by choosing from the given bricks. This 
+is a little harder than it looks and can be done without any loops. See 
+also: Introduction to MakeBricks"""
 
 def make_bricks(small, big, goal):
   needed = 0
@@ -16,116 +12,14 @@ def make_bricks(small, big, goal):
     needed = goal-big*5
     
   return small >= needed
-    
-# wow. this takes me a while
+
+"""Given 3 int values, a b c, return their sum. However, if one of the values 
+is the same as another of the values, it does not count towards the sum."""
+
 def lone_sum(a, b, c):
-  ol = [a, b, c]
-  nl = []
-  dl = []
-  
-  for n in ol:
-    if not n in nl:
-      nl.append(n)
-    else:
-      dl.append(n)
-      
-  sm = 0
-  for n in ol:
-    if n not in dl:
-      sm += n
-  
-  return sm
-
-def lone_sum_2(a, b, c):
-  ol = [a, b, c]
-  nl = []
-  dl = []
-  
-  for n in ol:
-    if not n in nl:
-      nl.append(n)
-    else:
-      dl.append(n)
-
-  fl = list(set(ol)-set(dl))      #nice. exactly i want to figure out
-
-  return sum(fl)
-
-# very interesting. code works on all cases on codingbat, but not working here
-# when a == b != c. doesn't make sense why it won't work
-def lone_sum_3(a, b, c):
-  ol = [a, b, c]
-  nl = []
-  fl = ol.copy()    #copy() not recognized on codingbat.com. used list(ol)
-  for n in ol:
-    if n not in nl:
-      nl.append(n)
-    else:
-      fl = filter(lambda a: a!=n, fl)
-      
-  return sum(fl)
-
-# does't work in these cases
-# print(lone_sum(3, 3, 7)) # → 0	0	OK	    
-# print(lone_sum(2, 2, 9)) # → 9	9	OK	
-
-# print(lone_sum(1, 2, 3)) # → 6	6	OK	
-# print(lone_sum(3, 2, 3)) # → 2	2	OK	
-# print(lone_sum(3, 3, 3)) # → 0	0	OK	
-# print(lone_sum(9, 2, 2)) # → 9	9	OK	
-# print(lone_sum(2, 9, 2)) # → 9	9	OK	
-# print(lone_sum(2, 9, 3)) # → 14	14	OK	
-# print(lone_sum(4, 2, 3)) # → 9	9	OK	
-# print(lone_sum(1, 3, 1)) # → 3	3	OK	
-
-# not scalable, but good for only 3 nbrs
-def lone_sum_online(a, b, c):
-  """
-  Given 3 int values, a b c, return their sum. However, if one of the values 
-  is the same as another of the values, it does not count towards the sum. 
-  """
-  sum = 0
-  sum += a if a not in [b,c] else 0
-  sum += b if b not in [a,c] else 0
-  sum += c if c not in [a,b] else 0
-  return sum
-
-# wow. very nice
-def lone_sum_5(a, b, c):
-  dct = {}
-  for x in [a,b,c]:
-    # or
-    # if not dct.has_key(x):
-    if x not in dct.keys():
-      dct[x] = 1
-    else:
-      dct[x] = dct[x] + 1
-  
-  sm = 0
-  for k,v in dct.items():
-    sm += k if v == 1 else 0
-    
-  return sm
-  # or even nicer
-  # return sum([k for k, v in dct.items() if v==1])
-
-def lone_sum_6(a, b, c):
   nums = [a, b, c]
-  set1 = set(nums)
-  temp = []
-  for n in set1:
-    if nums.count(n)==1:
-      temp.append(n)
-      
-  return sum(temp)
-
-# wow, wow. this is really nice
-def lone_sum_7(a, b, c):
-  nums = [a, b, c]
-  nums_set = set(nums)
   
-  return sum([n for n in nums_set if nums.count(n)==1])
-
+  return sum([n for n in nums if nums.count(n)==1])
 
 """Given 3 int values, a b c, return their sum. However, if one of the 
 values is 13 then it does not count towards the sum and values to its 
@@ -133,139 +27,41 @@ right do not count. So for example, if b is 13, then both b and c do not
 count."""
 
 def lucky_sum(a, b, c):
-  sm = 0
-  if a !=13:
-    sm += a
-  
-  if a != 13 and b != 13:
-    sm += b
-    
-  if a != 13 and b != 13 and c != 13:
-    sm += c
-    
-  return sm
-    
-def lucky_sum_2(a, b, c):
-  sm = 0
-  if a ==13:
-    sm = 0
-  
-  if a != 13 and b == 13:
-    sm = a
-    
-  if a != 13 and b != 13 and c == 13:
-    sm = a + b
+  nums = [a, b, c, 13]
 
-  if a != 13 and b != 13 and c != 13:
-    sm = a + b + c
+  return sum(nums[:nums.index(13)])
 
-  return sm
-
-# wow. this is pretty clever
-def lucky_sum_online(a, b, c):
-  sum = 0
-  list = [a,b,c,13]               #add 13 just in case there is no 13 in list
-  for n in list[:list.index(13)]:      #find the first index of 13 and use it to make a list from prior items
-    sum += n
-  return sum
-    
-def lucky_sum_my_revised(a, b, c):
-  ol = [a, b, c, 13]
-  nl = ol[:ol.index(13)]
-
-  return sum(nl)
-
-def lucky_sum(a, b, c):
-  ol = [a, b, c, 13]
-
-  return sum(ol[:ol.index(13)])
-    
-def lucky_sum_online2(a, b, c):
-  sm = 0
-  
-  for n in [a, b, c]:
-    if n!=13:
-      sm += n
-    else:
-      break
-
-  return sm
+"""Given 3 int values, a b c, return their sum. However, if any of the values 
+is a teen -- in the range 13..19 inclusive -- then that value counts as 0, 
+except 15 and 16 do not count as a teens. Write a separate helper "def 
+fix_teen(n):"that takes in an int value and returns that value fixed for 
+the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. 
+"decomposition"). Define the helper below and at the same indent level as the 
+main no_teen_sum()."""
     
 def no_teen_sum(a, b, c):
-  sm = 0
-  for n in list([a, b, c]):
-#   for n in [a, b, c]:       #this is fine too
-    if not fix_teen(n):
-      sm += n
-  
-  return sm
-  
-def fix_teen(n):
-  if 13 <= n <=19 and n not in [15, 16]:
-    return True
-  else:
-    return False
-
-def no_teen_sum_2(a, b, c):
-  return fix_teen(a) +  fix_teen(b) + fix_teen(c)
-  
-def fix_teen_2(n):
-  return n if n==15 or n==16 else 0 if 12<n<20 else n
-  # return 0 if 12<n<20 and n not in [15,16] else n    #better
-
-print(no_teen_sum(2, 13, 1))
-
-def no_teen_sum_3(a, b, c):
-  return sum([x for x in [a,b,c] if x not in range(13,15) and x not in range(17,20)])
-
-def no_teen_sum_4(a, b, c):
   return sum([n for n in [a,b,c] if n not in range(13, 20) or n in (15,16)])
 
-# nice
+"""For this problem, we'll round an int value up to the next multiple of 10 if 
+its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down 
+to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds 
+down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid 
+code repetition, write a separate helper "def round10(num):" and call it 3 times. 
+Write the helper entirely below and at the same indent level as round_sum()."""
+
 def round_sum(a, b, c):
-  sm = 0
-  for n in [a, b, c]:
-    sm += round10(n)
-    
-  return sm
-  
-def round10(num):
-  rmdr = num%10
-  if rmdr>=5:
-    return num + 10-rmdr
-  else:
-    return num - rmdr
-
-  # or 
-  # return n + 10-rmdr if rmdr>=5 else n - rmdr
-
-# somehow this doesn't work
-# def round_sum(a, b, c):
-#   return sum([x + 10 - x%10 for x in [a,b,c] if x%10>=5 else x - x%10])
-
-# but this works. wow
-def round_sum_2(a, b, c):
   return sum([x + 10 - x%10 if x%10>=5 else x- x%10 for x in [a,b,c]])
 
-def round_sum_3(a, b, c):
-  sm = 0
-  for n in [a, b, c]:
-    rmdr = n%10
-    if rmdr >= 5:
-      sm += n + 10 - rmdr
-    else:
-      sm += n - rmdr
-      
-  return sm
+"""Given three ints, a b c, return True if one of b or c is "close" (differing from 
+a by at most 1), while the other is "far", differing from both other values by 2 or 
+more. Note: abs(num) computes the absolute value of a number."""
 
 def close_far(a, b, c):
-  if (abs(a-b) <=1 or abs(a-c)<=1) and ((abs(a-b) >= 2 and abs(b-c)>=2) or (abs(a-c) >= 2 and abs(b-c)>=2)):
-    return True
-  else:
-    return False
-  
-def close_far(a, b, c):
   return abs(a-b)<=1 and abs(a-c)>=2 and abs(b-c)>=2 or abs(a-c)<=1 and abs(a-b)>=2 and abs(b-c)>=2
+
+"""We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) 
+and big bars (5 kilos each). Return the number of small bars to use, assuming we always 
+use big bars before small bars. Return -1 if it can't be done."""
 
 def make_chocolate(small, big, goal):
   needed = 0
@@ -275,17 +71,11 @@ def make_chocolate(small, big, goal):
   else:
     needed = goal%5
   
-  return -1 if small < needed else needed
+  return needed if small >= needed else -1
 
-def gcd(n, m):
-    """Compute the GCD of two integers by Euclid's algorithm."""
+def main():
+  pass
 
-    n, m = abs(n), abs(m)
-    n, m = min(n, m), max(n, m)  # Sort their absolute values.
+if __name__ == '__main__':
+  main()
 
-    if not n:
-      return m
-    
-    while m % n:         # While `n` doesn't divide into `m`:
-        n, m = m % n, n  # update the values of `n` amd `m`.
-    return n
--- Python/test_all.py
+++ Python/test_all.py
@@ -521,3 +521,138 @@ def test_lg1_near_ten():
    assert lg1.near_ten(3) == False	
    assert lg1.near_ten(1) == True
 
+def test_lg2_make_bricks():
+   assert lg2.make_bricks(3, 1, 8) == True
+   assert lg2.make_bricks(3, 1, 9) == False	
+   assert lg2.make_bricks(3, 2, 10) == True
+   assert lg2.make_bricks(3, 2, 8) == True
+   assert lg2.make_bricks(3, 2, 9) == False	
+   assert lg2.make_bricks(6, 1, 11) == True
+   assert lg2.make_bricks(6, 0, 11) == False	
+   assert lg2.make_bricks(1, 4, 11) == True
+   assert lg2.make_bricks(0, 3, 10) == True
+   assert lg2.make_bricks(1, 4, 12) == False	
+   assert lg2.make_bricks(3, 1, 7) == True
+   assert lg2.make_bricks(1, 1, 7) == False	
+   assert lg2.make_bricks(2, 1, 7) == True
+   assert lg2.make_bricks(7, 1, 11) == True
+   assert lg2.make_bricks(7, 1, 8) == True
+   assert lg2.make_bricks(7, 1, 13) == False	
+   assert lg2.make_bricks(43, 1, 46) == True
+   assert lg2.make_bricks(40, 1, 46) == False	
+   assert lg2.make_bricks(40, 2, 47) == True
+   assert lg2.make_bricks(40, 2, 50) == True
+   assert lg2.make_bricks(40, 2, 52) == False	
+   assert lg2.make_bricks(22, 2, 33) == False	
+   assert lg2.make_bricks(0, 2, 10) == True
+   assert lg2.make_bricks(1000000, 1000, 1000100) == True
+   assert lg2.make_bricks(2, 1000000, 100003) == False	
+   assert lg2.make_bricks(20, 0, 19) == True
+   assert lg2.make_bricks(20, 0, 21) == False	
+   assert lg2.make_bricks(20, 4, 51) == False	
+   assert lg2.make_bricks(20, 4, 39) == True
+
+def test_lg2_lone_sum():
+   assert lg2.lone_sum(1, 2, 3) == 6
+   assert lg2.lone_sum(3, 2, 3) == 2
+   assert lg2.lone_sum(3, 3, 3) == 0
+   assert lg2.lone_sum(9, 2, 2) == 9
+   assert lg2.lone_sum(2, 2, 9) == 9
+   assert lg2.lone_sum(2, 9, 2) == 9
+   assert lg2.lone_sum(2, 9, 3) == 14	
+   assert lg2.lone_sum(4, 2, 3) == 9
+   assert lg2.lone_sum(1, 3, 1) == 3
+
+def test_lg2_lucky_sum():
+   assert lg2.lucky_sum(1, 2, 3) == 6
+   assert lg2.lucky_sum(1, 2, 13) == 3
+   assert lg2.lucky_sum(1, 13, 3) == 1
+   assert lg2.lucky_sum(1, 13, 13) == 1
+   assert lg2.lucky_sum(6, 5, 2) == 13	
+   assert lg2.lucky_sum(13, 2, 3) == 0
+   assert lg2.lucky_sum(13, 2, 13) == 0
+   assert lg2.lucky_sum(13, 13, 2) == 0
+   assert lg2.lucky_sum(9, 4, 13) == 13	
+   assert lg2.lucky_sum(8, 13, 2) == 8
+   assert lg2.lucky_sum(7, 2, 1) == 10	
+   assert lg2.lucky_sum(3, 3, 13) == 6
+
+def test_lg2_no_teen_sum():
+   assert lg2.no_teen_sum(1, 2, 3) == 6
+   assert lg2.no_teen_sum(2, 13, 1) == 3
+   assert lg2.no_teen_sum(2, 1, 14) == 3
+   assert lg2.no_teen_sum(2, 1, 15) == 18	
+   assert lg2.no_teen_sum(2, 1, 16) == 19	
+   assert lg2.no_teen_sum(2, 1, 17) == 3
+   assert lg2.no_teen_sum(17, 1, 2) == 3
+   assert lg2.no_teen_sum(2, 15, 2) == 19	
+   assert lg2.no_teen_sum(16, 17, 18) == 16	
+   assert lg2.no_teen_sum(17, 18, 19) == 0
+   assert lg2.no_teen_sum(15, 16, 1) == 32	
+   assert lg2.no_teen_sum(15, 15, 19) == 30	
+   assert lg2.no_teen_sum(15, 19, 16) == 31	
+   assert lg2.no_teen_sum(5, 17, 18) == 5
+   assert lg2.no_teen_sum(17, 18, 16) == 16	
+   assert lg2.no_teen_sum(17, 19, 18) == 0
+
+def test_lg2_round_sum():
+   assert lg2.round_sum(16, 17, 18) == 60	
+   assert lg2.round_sum(12, 13, 14) == 30	
+   assert lg2.round_sum(6, 4, 4) == 10	
+   assert lg2.round_sum(4, 6, 5) == 20	
+   assert lg2.round_sum(4, 4, 6) == 10	
+   assert lg2.round_sum(9, 4, 4) == 10	
+   assert lg2.round_sum(0, 0, 1) == 0
+   assert lg2.round_sum(0, 9, 0) == 10	
+   assert lg2.round_sum(10, 10, 19) == 40	
+   assert lg2.round_sum(20, 30, 40) == 90	
+   assert lg2.round_sum(45, 21, 30) == 100	
+   assert lg2.round_sum(23, 11, 26) == 60	
+   assert lg2.round_sum(23, 24, 25) == 70	
+   assert lg2.round_sum(25, 24, 25) == 80	
+   assert lg2.round_sum(23, 24, 29) == 70	
+   assert lg2.round_sum(11, 24, 36) == 70	
+   assert lg2.round_sum(24, 36, 32) == 90	
+   assert lg2.round_sum(14, 12, 26) == 50	
+   assert lg2.round_sum(12, 10, 24) == 40
+
+def test_lg2_close_far():
+   assert lg2.close_far(1, 2, 10) == True
+   assert lg2.close_far(1, 2, 3) == False	
+   assert lg2.close_far(4, 1, 3) == True
+   assert lg2.close_far(4, 5, 3) == False	
+   assert lg2.close_far(4, 3, 5) == False	
+   assert lg2.close_far(-1, 10, 0) == True
+   assert lg2.close_far(0, -1, 10) == True
+   assert lg2.close_far(10, 10, 8) == True
+   assert lg2.close_far(10, 8, 9) == False	
+   assert lg2.close_far(8, 9, 10) == False	
+   assert lg2.close_far(8, 9, 7) == False	
+   assert lg2.close_far(8, 6, 9) == True
+
+def test_lg2_make_chocolate():
+   assert lg2.make_chocolate(4, 1, 9) == 4
+   assert lg2.make_chocolate(4, 1, 10) == -1	
+   assert lg2.make_chocolate(4, 1, 7) == 2
+   assert lg2.make_chocolate(6, 2, 7) == 2
+   assert lg2.make_chocolate(4, 1, 5) == 0
+   assert lg2.make_chocolate(4, 1, 4) == 4
+   assert lg2.make_chocolate(5, 4, 9) == 4
+   assert lg2.make_chocolate(9, 3, 18) == 3
+   assert lg2.make_chocolate(3, 1, 9) == -1	
+   assert lg2.make_chocolate(1, 2, 7) == -1	
+   assert lg2.make_chocolate(1, 2, 6) == 1
+   assert lg2.make_chocolate(1, 2, 5) == 0
+   assert lg2.make_chocolate(6, 1, 10) == 5
+   assert lg2.make_chocolate(6, 1, 11) == 6
+   assert lg2.make_chocolate(6, 1, 12) == -1	
+   assert lg2.make_chocolate(6, 1, 13) == -1	
+   assert lg2.make_chocolate(6, 2, 10) == 0
+   assert lg2.make_chocolate(6, 2, 11) == 1
+   assert lg2.make_chocolate(6, 2, 12) == 2
+   assert lg2.make_chocolate(60, 100, 550) == 50	
+   assert lg2.make_chocolate(1000, 1000000, 5000006) == 6
+   assert lg2.make_chocolate(7, 1, 12) == 7
+   assert lg2.make_chocolate(7, 1, 13) == -1	
+   assert lg2.make_chocolate(7, 2, 13) == 3
+
